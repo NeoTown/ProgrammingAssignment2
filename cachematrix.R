048b6d99672ba8fc3265961d038035a9bbbf19cd
Nilton <niltonfagner@gmail.com>
2014-05-24 18:13:24 -0400

    cache matrix with example

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is usually a costly computation and their may be some benefit
+## to caching the inverse of a matrix rather than compute it repeatedly. Bellow are
+## two functions that are used to create a special object that stores a matrix and
+## cache's its inverse.
 
-## Write a short comment describing this function
+## The first function, makeCacheMatrix creates a list containing a function to
+## 1. set the value of the matrix
+## 2. get the value of the matrix
+## 3. set the value of inverse
+## 4. get the value of inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y){
+    x <<- y
+    i <<- NULL
+  }
+  
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set=set, get=get,
+       setinverse=setinverse,
+       getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+
+## The following function calculates the inverse of the matrix created with the above
+## function. It first checks if the inverse has already been calculated. If so, it gets
+## the result and skips the computation. Otherwise, it calculates the inverse, sets the
+## value in the cache via setinverse function.
+
+## This function assumes that the matrix is always invertible.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+ 
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data.")
+    return(i)
+  }
+  
+  data <- x$get()
+  i <- solve(data)
+  x$setinverse(i)
+  i
 }
+
+## Example:
+## x <- matrix(rnorm(4), nrow = 2)  //Create a matrix 'x'
+## cm <- makeCacheMatrix(x)         //Special matrix
+## cacheSolve(cm)                   //Inverse of the matrix
+## cacheSolve(cm)                   //Return cached inverse
